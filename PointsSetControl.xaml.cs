8bca70214f925a8c987c37b061fa02e9aef41b52
Denis Lambolez <denis.lambolez@gmail.com>
2020-04-12 16:47:07 +0200

    Events and Properties
    Surface Canvas CreateResources event at PointsSetControl level and make ScaleFactor a property of PointsSetControl

--- PointsSetControl.xaml.cs
+++ PointsSetControl.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.Graphics.Canvas.UI;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Windows.ApplicationModel.Resources;
 using CatsHelpers;
+using System.ComponentModel;
 
 // Pour en savoir plus sur le modèle d'élément Contrôle utilisateur, consultez la page https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -54,8 +55,47 @@ namespace CatsControls
             colorArrayPool = ArrayPool<Color>.Shared;
             doubleArrayPool = ArrayPool<double>.Shared;
 
+            // Scale can't be zero
             _scale = 1;
         }
+        #endregion
+          
+        #region UserControl Properties
+        [Browsable(false)]
+        public Point Origin
+        {
+            get => _origin;
+            set
+            {
+                _origin = value;
+
+                Calculate();
+                Render();
+                Canvas.Invalidate();
+            }
+        }
+
+        [Browsable(true)] [Category("Behavior")]
+        [Description("Define scale factor for the complex plan.")]
+        public double ScaleFactor
+        {
+            get => _scale;
+            set
+            {
+                if (value == 0) throw new ArgumentOutOfRangeException(nameof(ScaleFactor), resourceLoader.GetString("ValueNotZero"));
+                _scale = value;
+
+                Calculate();
+                Render();
+                Canvas.Invalidate();
+            }
+        }
+        #endregion
+
+        #region UserControl Events
+        [Browsable(true)] [Category("Action")]
+        [Description("Invoked when PointsSet is ready to draw.")]
+        public event TypedEventHandler<CanvasControl, CanvasCreateResourcesEventArgs> CreateResources;
         #endregion
 
         #region UserControl Methods
@@ -74,35 +114,6 @@ namespace CatsControls
             Calculate();
             Render();
             Canvas.Invalidate();
-        }
-        #endregion
-
-        #region UserControl Properties
-        public Point Origin
-        {
-            get => _origin;
-            set
-            {
-                _origin = value;
-
-                Calculate();
-                Render();
-                Canvas.Invalidate();
-            }
-        }
-
-        public double ScaleFactor
-        {
-            get => _scale;
-            set
-            {
-                if (value == 0) throw new ArgumentOutOfRangeException(nameof(ScaleFactor), resourceLoader.GetString("ValueNotZero"));
-                _scale = value;
-
-                Calculate();
-                Render();
-                Canvas.Invalidate();
-            }
         }
         #endregion
 
@@ -175,11 +186,13 @@ namespace CatsControls
             height = sender.ActualHeight;
             size = sender.ActualSize.ToSize();
             pointsCount = Convert.ToInt32(width * height);
-            center = new Point(width / 2, height / 2);
-            
+            center = new Point(width / 2, height / 2);           
             if (args.Reason == CanvasCreateResourcesReason.FirstTime) _origin = center;
 
             AllocateRenderTarget();
+
+            // Call PointsSet CreateResources event before entering the pipeline
+            CreateResources?.Invoke(sender, args);
             Calculate();
             Render();
             Canvas.Invalidate();
